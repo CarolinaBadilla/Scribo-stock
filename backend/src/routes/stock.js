968ef4e8e40b0e427abb6e7af4785d32c1c2c2ef
Carolina <carolinabadilla333@gmail.com>
2026-08-21 13:36:27 -0300

    cambios en ruta de stock

--- backend/src/routes/stock.js
+++ backend/src/routes/stock.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const stockController = require('../controllers/stockController');
 
 router.get('/por-sucursal/:id', stockController.obtenerStockPorSucursal);
-router.post('/agregar', stockController.agregarStock);
 
 module.exports = router;
