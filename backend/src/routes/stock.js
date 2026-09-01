6f99ae75af47521939441942943ba1efb5d68242
Carolina <carolinabadilla333@gmail.com>
2026-09-01 20:29:09 -0300

    arreglo de funcion stock

--- backend/src/routes/stock.js
+++ backend/src/routes/stock.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const stockController = require('../controllers/stockController');
 
 router.get('/por-sucursal/:id', stockController.obtenerStockPorSucursal);
-router.get('/alertas', stockController.obtenerAlertasStock);
+router.get('/alertas', stockController.obtenerAlertasStockBajo);
 
 module.exports = router;
